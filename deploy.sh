98d674b77c97207a8c1ec05dace992f406a408a9
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-07-08 16:12:11 +0200

    Fix locale in images

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -eux
 docker push h1cr.io/website/alpine-shell:latest
+docker push h1cr.io/website/debian-shell:latest
 docker push h1cr.io/website/nginx-static:latest
 docker push h1cr.io/website/php-apache:5.6
 docker push h1cr.io/website/php-apache:7.2
--- php-apache-shell/test/php.bats
+++ php-apache-shell/test/php.bats
@@ -4,3 +4,8 @@
 	run docker run --rm "$IMAGE" h1 --version
 	[ "$status" -eq 0 ]
 }
+
+@test "run sftp-server in image" {
+	run docker run --rm "$IMAGE" sftp-server --help
+	[ "$status" -eq 0 ]
+}
