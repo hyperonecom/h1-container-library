885831764ef85217ae65b5755321764732efb82d
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-05-11 19:11:15 +0200

    Improve deployment on CI

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 set -eux;
 echo "$DOCKER_PASSWORD" | docker login "$DOCKER_REGISTRY" -u "$DOCKER_USERNAME" --password-stdin;
 NODE=$(which node)
-sudo $NODE ./generate.js "$FAMILY" -d
+sudo $NODE ./generate.js -f "$FAMILY" -d
