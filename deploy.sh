2e1267f362bae04709b2a536a89382daedac14f9
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-05-11 12:02:05 +0200

    Fix sudo in deployment container

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -eux;
 echo "$DOCKER_PASSWORD" | docker login "$DOCKER_REGISTRY" -u "$DOCKER_USERNAME" --password-stdin;
-sudo node ./generate.js "$FAMILY" -d
+NODE=$(which node)
+sudo $NODE ./generate.js "$FAMILY" -d
